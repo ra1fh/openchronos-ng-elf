76d3c89cb24e46448a1781c369eea00a28f21608
Ralf Horstmann <ralf+github@ackstorm.de>
2017-10-29 14:56:11 +0100

    Make altitude module work and add QNH setting
    The altitude module has three screens new that can be
    switched with '#':
    
    - altitude in feet
    - altitude in meters
    - pressure in hPa
    
    The altitude is calculated from pressure according to ISA
    standard atmosphere with configurable QNH representing pressure
    at sea level. The calculation is probably not very efficient
    and has a high code size overhead by requiring libm. To be
    improved later...
    
    To set QNH, long press '*' and set QNH with up and down keys.
    Default QNH is 1013. To find your current QNH, lookup METAR
    information of closest airport.

--- modules/altitude.c
+++ modules/altitude.c
@@ -21,6 +21,8 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 **/
 
+#include <math.h>
+
 #include "messagebus.h"
 #include "menu.h"
 
@@ -30,55 +32,103 @@
 #include "drivers/vti_ps.h"
 
 extern uint8_t ps_ok;
-static uint8_t altitude_dots;
+static uint8_t altitude_dots = 0;
+static uint8_t altitude_screen = 0;
+static uint32_t altitude_qnh_cur = 1013;
+static uint32_t altitude_qnh_tmp = 1013;
+
+static void altitude_print_line2(int display, uint32_t val, uint32_t scale)
+{
+	uint32_t digits[10] = { 0 };
+	uint32_t v = val;
+	const char *fmt = "%2u";
+	int i;
+	
+	for (i = 0; i < (sizeof(digits) / sizeof(digits[0])); ++i) {
+		digits[i] = v % 10;
+		v  = v / 10;
+	}
+
+	if (val >= 10000) {
+		_printf(display, LCD_SEG_L2_5_4, fmt, digits[5 + scale] * 10 + digits[4 + scale]);
+		fmt = "%02u";
+	} else {
+		display_chars(display, LCD_SEG_L2_5_4, "  ", SEG_SET);
+	}
+	if (val >= 100) {
+		_printf(display, LCD_SEG_L2_3_2, fmt, digits[3 + scale] * 10 + digits[2 + scale]);
+		fmt = "%02u";
+	} else {
+		display_chars(display, LCD_SEG_L2_3_2, "  ", SEG_SET);
+	}
+	_printf(display, LCD_SEG_L2_1_0, fmt, digits[1 + scale] * 10 + digits[0 + scale]);
+}
 
 static void altitude_event(enum sys_message msg)
 {
-	uint32_t pressure;
-	uint8_t digits[10] = { 0 };
-	int i;
+	uint32_t p_meas;
+	uint32_t p_disp;
+	uint32_t alt;
+	double qnh = altitude_qnh_cur;
 
 	altitude_dots = !altitude_dots;
 	display_symbol(0, LCD_SYMB_ARROW_UP,   altitude_dots ? SEG_ON  : SEG_OFF);
 	display_symbol(0, LCD_SYMB_ARROW_DOWN, altitude_dots ? SEG_OFF : SEG_ON );
-	display_symbol(0, LCD_SEG_L2_DP, SEG_ON);
+	display_symbol(1, LCD_SYMB_ARROW_UP,   altitude_dots ? SEG_ON  : SEG_OFF);
+	display_symbol(1, LCD_SYMB_ARROW_DOWN, altitude_dots ? SEG_OFF : SEG_ON );
+	display_symbol(2, LCD_SYMB_ARROW_UP,   altitude_dots ? SEG_ON  : SEG_OFF);
+	display_symbol(2, LCD_SYMB_ARROW_DOWN, altitude_dots ? SEG_OFF : SEG_ON );
 
-	pressure = ps_get_pa();
-	for (i = 0; i < sizeof(digits); ++i) {
-		digits[i] = pressure % 10;
-		pressure  = pressure / 10;
-	}
+	p_meas = p_disp = ps_get_pa();
 
-	i = 0;
-	_printf(0, LCD_SEG_L2_1_0, "%02u", digits[1 + i] * 10 + digits[0 + i]);
-	_printf(0, LCD_SEG_L2_3_2, "%02u", digits[3 + i] * 10 + digits[2 + i]);
-	_printf(0, LCD_SEG_L2_5_4, "%02u", digits[5 + i] * 10 + digits[4 + i]);
-}
+	alt = 145442.2 * (1.0 - pow(((double)p_meas) / qnh / 100.0, 0.19));
+	altitude_print_line2(0, alt, 0);
 
-/* update screens with fake event */
-static inline void update_screen()
-{
-    altitude_event(SYS_MSG_RTC_SECOND);
+	alt = 44330.8 * (1.0 - pow(((double)p_meas) / qnh / 100.0, 0.19));
+	altitude_print_line2(1, alt, 0);
+
+	altitude_print_line2(2, p_meas, 0);
 }
 
 /************************ menu callbacks **********************************/
 static void altitude_activated()
 {
-    /* create one screens */
-    lcd_screens_create(1); // 0: log, 1: show, 2: diff
+	/* 0: Pressure [hPA]
+     * 1: Altitude [ft]
+	 * 2: Altitude [m]
+	 * 3: QNH Setting
+	 */
+    lcd_screens_create(4);
 
 	display_chars(0,  LCD_SEG_L1_3_0, "ALTI", SEG_SET);
+	display_symbol(0, LCD_UNIT_L1_FT, SEG_ON);
+	
+	display_chars(1,  LCD_SEG_L1_3_0, "ALTI", SEG_SET);
+	display_symbol(1, LCD_UNIT_L1_M, SEG_ON);
 
+	display_chars(2,  LCD_SEG_L1_3_0, " HPA", SEG_SET);
+	display_symbol(2, LCD_SEG_L2_DP, SEG_ON);
+
+	display_chars(3,  LCD_SEG_L1_3_0, " QNH", SEG_SET);
+	_printf(3, LCD_SEG_L2_3_0, "%4u", altitude_qnh_cur);
+	
     sys_messagebus_register(&altitude_event, SYS_MSG_PS_INT);
 
 	if (ps_ok) {
 		display_chars(0,  LCD_SEG_L2_4_0, "OK   ", SEG_SET);
+		display_chars(1,  LCD_SEG_L2_4_0, "OK   ", SEG_SET);
+		display_chars(2,  LCD_SEG_L2_4_0, "OK   ", SEG_SET);
 	} else {
 		display_chars(0,  LCD_SEG_L2_4_0, "ERR 0", SEG_SET);
+		display_chars(1,  LCD_SEG_L2_4_0, "ERR 0", SEG_SET);
+		display_chars(2,  LCD_SEG_L2_4_0, "ERR 0", SEG_SET);
 	}
 
 	/* enable ps */
 	ps_start();
+
+	altitude_screen = 0;
+	lcd_screen_activate(altitude_screen);
 }
 
 static void altitude_deactivated()
@@ -92,12 +142,15 @@ static void altitude_deactivated()
     lcd_screens_destroy();
 
     /* clean up screen */
-    display_clear(0, 1);
-    display_clear(0, 2);
+    display_clear(0, 0);
 }
 
 static void num_pressed()
 {
+	altitude_screen += 1;
+	if (altitude_screen > 2)
+		altitude_screen = 0;
+	lcd_screen_activate(altitude_screen);
 }
 
 static void num_long_pressed()
@@ -112,13 +165,60 @@ static void down_pressed()
 {
 }
 
+/************************ edit mode **********************************/
+
+static void altitude_print_qnh(void)
+{
+	_printf(3, LCD_SEG_L2_3_0, "%4u", altitude_qnh_tmp);
+}
+
+static void altitude_edit_sel(void)
+{
+    display_chars(3, LCD_SEG_L2_3_0, NULL, BLINK_ON);
+}
+
+static void altitude_edit_dsel(void)
+{
+    display_chars(3, LCD_SEG_L2_3_0, NULL, BLINK_OFF);
+}
+
+static void altitude_edit_set(int8_t step)
+{
+	altitude_qnh_tmp += step;
+	if (altitude_qnh_tmp < 900)
+		altitude_qnh_tmp = 900;
+	if (altitude_qnh_tmp > 1100)
+		altitude_qnh_tmp = 1100;
+    altitude_print_qnh();
+}
+
+static struct menu_editmode_item altitude_edit_items[] = {
+    {&altitude_edit_sel, &altitude_edit_dsel, &altitude_edit_set},
+    { NULL },
+};
+
+static void altitude_edit_save(void)
+{
+	altitude_qnh_cur = altitude_qnh_tmp;
+	lcd_screen_activate(altitude_screen);
+}
+
+static void star_long_pressed()
+{
+    altitude_print_qnh();
+	lcd_screen_activate(3);
+	altitude_qnh_tmp = altitude_qnh_cur;
+    menu_editmode_start(&altitude_edit_save, altitude_edit_items);
+}
+
+/************************ init **********************************/
 void mod_altitude_init()
 {
     menu_add_entry("ALTI",
 					&up_pressed,           	 /* up         */
 					&down_pressed,         	 /* down       */
 					&num_pressed,          	 /* num        */
-					NULL,                  	 /* star long  */
+					&star_long_pressed,		 /* star long  */
 					&num_long_pressed,     	 /* num long   */
 					NULL,                  	 /* up + down  */
 					&altitude_activated,     /* activate   */
